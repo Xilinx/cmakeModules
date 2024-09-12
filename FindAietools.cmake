48d84e03b5dcae7022f0b9f6e352dcc1eff7a792
Kristof Denolf <kristof.denolf@amd.com>
2024-09-12 13:51:14 -0600

    fix typo

--- FindAietools.cmake
+++ FindAietools.cmake
@@ -1,6 +1,6 @@
 ###############################################################################
 #  Copyright (c) 2024, AMD, Inc.
-#  All rights recd cd cd served.
+#  All rights reserved.
 # 
 #  Redistribution and use in source and binary forms, with or without 
 #  modification, are permitted provided that the following conditions are met:
